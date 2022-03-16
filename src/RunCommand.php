7c94478392a9695073fc1164081484c8f1860e46
Subhojit Paul <subhojit.paul@axelerant.com>
2022-03-16 13:36:19 +0530

    code formatted as per psr2

--- src/RunCommand.php
+++ src/RunCommand.php
@@ -6,16 +6,20 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class RunCommand extends Command {
+class RunCommand extends Command
+{
+    protected function configure(): void
+    {
+        $this->setName('run')
+          ->setDescription('Compare the source and destination data.');
+    }
 
-  protected function configure(): void {
-    $this->setName('run')
-      ->setDescription('Compare the source and destination data.');
-  }
+    protected function execute(
+        InputInterface $input,
+        OutputInterface $output
+    ): int {
+        $output->writeln("Hello world!");
 
-  protected function execute(InputInterface $input, OutputInterface $output): int {
-    $output->writeln("Hello world!");
-    return Command::SUCCESS;
-  }
-
+        return Command::SUCCESS;
+    }
 }
