cb45ca79f0195ca402737057cad51c4f229a6a52
Subhojit Paul <subhojit.paul@axelerant.com>
2022-03-21 12:21:38 +0530

    load test definitions from specific directory

--- src/LoadDefinition/DefaultLoader.php
+++ src/LoadDefinition/DefaultLoader.php
@@ -11,12 +11,12 @@ class DefaultLoader implements Load
      */
     public function scan(): iterable
     {
-        $ymlTestDefinitions = glob("tests/*.yml", GLOB_ERR);
+        $ymlTestDefinitions = glob("tests/mdtt/*.yml", GLOB_ERR);
         if ($ymlTestDefinitions === false) {
             throw new IOException("Error occurred while loading test definitions");
         }
 
-        $yamlTestDefinitions = glob("tests/*.yaml", GLOB_ERR);
+        $yamlTestDefinitions = glob("tests/mdtt/*.yaml", GLOB_ERR);
         if ($yamlTestDefinitions === false) {
             throw new IOException("Error occurred while loading test definitions");
         }
