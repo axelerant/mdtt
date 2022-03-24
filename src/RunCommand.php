31380241376380e21243cfeaee9aa97c3645d275
Subhojit Paul <subhojit.paul@axelerant.com>
2022-03-24 18:22:32 +0530

    logger service

--- src/RunCommand.php
+++ src/RunCommand.php
@@ -5,14 +5,22 @@ declare(strict_types=1);
 namespace Mdtt;
 
 use Mdtt\LoadDefinition\DefaultLoader;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Logger\ConsoleLogger;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Filesystem\Exception\IOException;
 
 class RunCommand extends Command
 {
+    private LoggerInterface $logger;
+
+    public function __construct(LoggerInterface $logger, string $name = null)
+    {
+        parent::__construct($name);
+        $this->logger = $logger;
+    }
+
     protected function configure(): void
     {
         $this->setName('run')
@@ -23,10 +31,8 @@ class RunCommand extends Command
         InputInterface $input,
         OutputInterface $output
     ): int {
-        $logger = new ConsoleLogger($output);
-
         try {
-            $logger->info("Loading test definitions");
+            $this->logger->info("Loading test definitions");
 
             /** @var \Mdtt\Definition\Definition[] $definitions */
             $definitions = (new DefaultLoader())->validate();
@@ -34,7 +40,7 @@ class RunCommand extends Command
                 $definition->runTests();
             }
         } catch (IOException $exception) {
-            $logger->error($exception->getMessage());
+            $this->logger->error($exception->getMessage());
             return Command::FAILURE;
         }
 
