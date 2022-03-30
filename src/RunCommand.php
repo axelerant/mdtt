e4b726ec3eddcc32efedd1dfd65f6fe556771846
Subhojit Paul <subhojit.paul@axelerant.com>
2022-03-30 13:14:06 +0530

    pass email as option

--- src/RunCommand.php
+++ src/RunCommand.php
@@ -9,6 +9,7 @@ use Mdtt\LoadDefinition\DefaultLoader;
 use Mdtt\Notification\Email;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Logger\ConsoleLogger;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Filesystem\Exception\IOException;
@@ -26,7 +27,13 @@ class RunCommand extends Command
     protected function configure(): void
     {
         $this->setName('run')
-          ->setDescription('Compare the source and destination data.');
+            ->setDescription('Compare the source and destination data.')
+            ->addOption(
+                'email',
+                null,
+                InputOption::VALUE_REQUIRED,
+                'The email address where the notification will be sent when test completes.'
+            );
     }
 
     protected function execute(
@@ -47,10 +54,12 @@ class RunCommand extends Command
             return Command::FAILURE;
         }
 
-        try {
-            $this->email->sendMessage("Test completed", "Test completed", "subhojit.paul@axelerant.com");
-        } catch (SetupException $exception) {
-            $logger->error($exception->getMessage());
+        if ($input->getOption('email') !== null) {
+            try {
+                $this->email->sendMessage("Test completed", "Test completed", $input->getOption('email'));
+            } catch (SetupException $exception) {
+                $logger->error($exception->getMessage());
+            }
         }
 
         return Command::SUCCESS;
