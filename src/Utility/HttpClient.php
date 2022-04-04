2f37532fb3c78ef39ddacc5b4a67f6a0e0048d1b
Subhojit Paul <subhojit.paul@axelerant.com>
2022-04-04 13:49:03 +0530

    destination datasource using generator

--- src/Utility/HttpClient.php
+++ src/Utility/HttpClient.php
@@ -4,17 +4,17 @@ declare(strict_types=1);
 
 namespace Mdtt\Utility;
 
-use Symfony\Component\HttpClient\HttpClient as MainHttpClient;
-use Symfony\Contracts\HttpClient\HttpClientInterface;
+use GuzzleHttp\Client;
+use Psr\Http\Client\ClientInterface;
 
 class HttpClient
 {
-    private HttpClientInterface $client;
+    private ClientInterface $client;
 
-    public function getClient(): HttpClientInterface
+    public function getClient(): ClientInterface
     {
         if (!isset($this->client)) {
-            $this->client = MainHttpClient::create();
+            $this->client = new Client();
         }
 
         return $this->client;
