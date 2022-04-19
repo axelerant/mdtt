174644df1b5bc413f4fcfc26c943c49c29d128bc
Subhojit Paul <subhojit.paul@axelerant.com>
2022-04-19 16:32:58 +0530

    missing id test case

--- tests/LoadDefinition/LoadTest.php
+++ tests/LoadDefinition/LoadTest.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace Mdtt\tests\LoadDefinition;
 
 use Mdtt\Definition\Validate\DataSource\Validator;
+use Mdtt\Exception\SetupException;
 use Mdtt\LoadDefinition\DefaultLoader;
+use Mdtt\LoadDefinition\Load;
 use Mdtt\Transform\PluginManager;
 use PHPUnit\Framework\TestCase;
 use Prophecy\PhpUnit\ProphecyTrait;
@@ -19,6 +21,7 @@ class LoadTest extends TestCase
     private ObjectProphecy $logger;
     private ObjectProphecy $validator;
     private ObjectProphecy $transformationPluginManager;
+    private Load $defaultLoader;
 
     protected function setUp(): void
     {
@@ -26,22 +29,45 @@ class LoadTest extends TestCase
         $this->validator = $this->prophesize(Validator::class);
         $this->transformationPluginManager = $this->prophesize(PluginManager::class);
 
+        $this->defaultLoader = new DefaultLoader(
+          $this->logger->reveal(),
+          $this->validator->reveal(),
+          $this->transformationPluginManager->reveal()
+        );
+
         parent::setUp();
     }
 
     public function testScan(): void
     {
-        $defaultLoader = new DefaultLoader(
-            $this->logger->reveal(),
-            $this->validator->reveal(),
-            $this->transformationPluginManager->reveal()
-        );
-
-        $rawTestDefinitions = $defaultLoader->scan([
+        $rawTestDefinitions = $this->defaultLoader->scan([
           "tests/fixtures/*.yml"
         ]);
 
         self::assertNotEmpty($rawTestDefinitions);
         self::assertCount(2, $rawTestDefinitions);
+    }
+
+    /**
+     * @dataProvider validateExceptionDataProvider
+     */
+    public function testValidateException(array $testDefinition, string $exceptionMessage): void
+    {
+        $this->expectException(SetupException::class);
+        $this->expectExceptionMessage($exceptionMessage);
+
+        $this->defaultLoader->validate($testDefinition);
+    }
+
+    public function validateExceptionDataProvider(): array
+    {
+        return [
+          [
+              [
+                "tests/fixtures/negative/validate_public_apis_1.yml"
+              ],
+              "Test definition id is missing"
+          ]
+        ];
     }
 }
