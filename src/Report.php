b338534ad83e185108e408e4cc3537385bdc697d
Subhojit Paul <subhojit.paul@axelerant.com>
2022-04-20 16:52:04 +0530

    init report

---
+++ src/Report.php
@@ -0,0 +1,100 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mdtt;
+
+class Report
+{
+    private int $numberOfTestDefinitions = 0;
+    private int $numberOfAssertions = 0;
+    private int $numberOfFailures = 0;
+    private int $sourceRowCount = 0;
+    private int $destinationRowCount = 0;
+
+    /**
+     * @param int $numberOfTestDefinitions
+     */
+    public function setNumberOfTestDefinitions(
+        int $numberOfTestDefinitions
+    ): void {
+        $this->numberOfTestDefinitions = $numberOfTestDefinitions;
+    }
+
+    /**
+     * @param int $numberOfAssertions
+     */
+    public function setNumberOfAssertions(int $numberOfAssertions): void
+    {
+        $this->numberOfAssertions = $numberOfAssertions;
+    }
+
+    /**
+     * @param int $numberOfFailures
+     */
+    public function setNumberOfFailures(int $numberOfFailures): void
+    {
+        $this->numberOfFailures = $numberOfFailures;
+    }
+
+    /**
+     * @param int $sourceRowCount
+     */
+    public function setSourceRowCount(int $sourceRowCount): void
+    {
+        $this->sourceRowCount = $sourceRowCount;
+    }
+
+    /**
+     * @param int $destinationRowCount
+     */
+    public function setDestinationRowCount(int $destinationRowCount): void
+    {
+        $this->destinationRowCount = $destinationRowCount;
+    }
+
+    /**
+     * @return int
+     */
+    public function getNumberOfTestDefinitions(): int
+    {
+        return $this->numberOfTestDefinitions;
+    }
+
+    /**
+     * @return int
+     */
+    public function getNumberOfAssertions(): int
+    {
+        return $this->numberOfAssertions;
+    }
+
+    /**
+     * @return int
+     */
+    public function getNumberOfFailures(): int
+    {
+        return $this->numberOfFailures;
+    }
+
+    /**
+     * @return int
+     */
+    public function getSourceRowCount(): int
+    {
+        return $this->sourceRowCount;
+    }
+
+    /**
+     * @return int
+     */
+    public function getDestinationRowCount(): int
+    {
+        return $this->destinationRowCount;
+    }
+
+    public function isFailure(): bool
+    {
+        return ($this->numberOfFailures) === 0 && ($this->sourceRowCount === $this->destinationRowCount);
+    }
+}
